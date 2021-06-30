389740a964ad3a97dea81407ef33ff7ca075afb3
leocuba88 <leocuba88@hotmail.com>
2021-06-30 04:17:33 +0000

    doing

--- src/app.js
+++ src/app.js
@@ -8,4 +8,23 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  function genRandomDomain() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let domain = [".com", ".net", ".us", ".io"];
+    var totalDomain = pronoun.concat(adj, noun, domain);
+    for (let i = 0; i < totalDomain.length; i++) {
+      var randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+      var randomAdj = adj[Math.floor(Math.random() * adj.length)];
+      var randomNoun = noun[Math.floor(Math.random() * noun.length)];
+      var randomDomain = domain[Math.floor(Math.random() * domain.length)];
+      var result = randomPronoun + randomAdj + randomNoun + randomDomain;
+      console.log(result);
+    }
+    return result;
+  }
+  const renderExcuse = () => {
+    document.querySelector("#excuse").innerHTML = genRandomDomain();
+  };
 };
