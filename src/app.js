6bb0668ab8a9a78d18fafd12153185b41fa9faf0
leocuba88 <leocuba88@hotmail.com>
2021-07-07 00:39:22 +0000

    done

--- src/app.js
+++ src/app.js
@@ -14,17 +14,20 @@ window.onload = function() {
     let noun = ["jogger", "racoon"];
     let domain = [".com", ".net", ".us", ".io"];
     var totalDomain = pronoun.concat(adj, noun, domain);
+    let result;
     for (let i = 0; i < totalDomain.length; i++) {
       var randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
       var randomAdj = adj[Math.floor(Math.random() * adj.length)];
       var randomNoun = noun[Math.floor(Math.random() * noun.length)];
       var randomDomain = domain[Math.floor(Math.random() * domain.length)];
-      var result = randomPronoun + randomAdj + randomNoun + randomDomain;
-      console.log(result);
+      result = randomPronoun + randomAdj + randomNoun + randomDomain;
     }
     return result;
   }
-  const renderExcuse = () => {
-    document.querySelector("#excuse").innerHTML = genRandomDomain();
+  const genDomain = () => {
+    document.querySelector("#domain1").innerHTML = genRandomDomain();
+    document.querySelector("#domain2").innerHTML = genRandomDomain();
+    document.querySelector("#domain3").innerHTML = genRandomDomain();
   };
+  genDomain();
 };
