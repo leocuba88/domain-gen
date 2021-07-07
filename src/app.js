28f38475f9463d4d592f40be8b1dbc5735511ee3
leocuba88 <leocuba88@hotmail.com>
2021-07-07 01:12:15 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,33 +1,36 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-  function genRandomDomain() {
-    let pronoun = ["the", "our"];
-    let adj = ["great", "big"];
-    let noun = ["jogger", "racoon"];
-    let domain = [".com", ".net", ".us", ".io"];
-    var totalDomain = pronoun.concat(adj, noun, domain);
-    let result;
-    for (let i = 0; i < totalDomain.length; i++) {
-      var randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
-      var randomAdj = adj[Math.floor(Math.random() * adj.length)];
-      var randomNoun = noun[Math.floor(Math.random() * noun.length)];
-      var randomDomain = domain[Math.floor(Math.random() * domain.length)];
-      result = randomPronoun + randomAdj + randomNoun + randomDomain;
+function generateLi(domain) {
+  return "<LI>" + domain + "</LI>";
+}
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".io", ".us"];
+function generateDomains(pronounArr, adjArr, NounArr, extensionArr) {
+  let storageArr = [];
+  for (let i = 0; i < pronounArr.length; i++) {
+    for (let j = 0; j < adjArr.length; j++) {
+      for (let k = 0; k < NounArr.length; k++) {
+        for (let e = 0; e < extensionArr.length; e++) {
+          storageArr.push(
+            pronounArr[i] + adjArr[j] + NounArr[k] + extensionArr[e]
+          );
+        }
+      }
     }
-    return result;
   }
-  const genDomain = () => {
-    document.querySelector("#domain1").innerHTML = genRandomDomain();
-    document.querySelector("#domain2").innerHTML = genRandomDomain();
-    document.querySelector("#domain3").innerHTML = genRandomDomain();
-  };
-  genDomain();
+  let arrayOfLiTags = storageArr.map(function(item, index) {
+    return generateLi(item);
+  });
+  let result = arrayOfLiTags.join("");
+  return result;
+}
+window.onload = function() {
+  const unorderedList = document.getElementById("list-of-domains");
+  unorderedList.innerHTML = generateDomains(pronoun, adj, noun, extension);
+  console.log("Hello Rigo from the console!");
 };
